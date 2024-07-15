e4182b03ea271a95e97c90285f4324d8d9613575
Unstream <drns.pw@icloud.com>
2024-07-14 21:02:32 -0400

    network fetch

--- index.ts
+++ index.ts
@@ -11,43 +11,40 @@ import {
 } from "@api/MessageEvents";
 import definePlugin from "@utils/types";
 
-import { dictionary } from "./dictionary.ts";
+const getPresend = (dictionary) => {
+  const presendObject: SendListener = (_, msg) => {
+    msg.content = msg.content.trim();
+    if (!msg.content.includes("```") && /\w/.test(msg.content.charAt(0))) {
+      const re = new RegExp(
+        `(^|(?<=[^A-Z0-9]+))(${Object.keys(dictionary)
+          .map((k) => k.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+          .join("|")})((?=[^A-Z0-9]+)|$)`,
+        "gi"
+      );
+      if (re !== null) {
+        msg.content = msg.content.replace(re, (match) => {
+          return dictionary[match.toLowerCase()] || match;
+        });
+      }
 
-const regex = () => {
-  const keywords = Object.keys(dictionary).map((k) =>
-    k.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
-  );
-  if (!keywords.length) return null;
-  return new RegExp(
-    `(^|(?<=[^A-Z0-9]+))(${keywords.join("|")})((?=[^A-Z0-9]+)|$)`,
-    "gi"
-  );
-};
+      if (/[A-Z0-9]/i.test(msg.content.charAt(msg.content.length - 1))) {
+        if (!msg.content.startsWith("http", msg.content.lastIndexOf(" ") + 1))
+          msg.content += ".";
+      }
 
-const presendObject: SendListener = (_, msg) => {
-  if (!msg.content.includes("```") && /\w/.test(msg.content.charAt(0))) {
-    const re = regex();
-    if (re !== null) {
-      msg.content = msg.content.replace(re, (match) => {
-        return dictionary[match.toLowerCase()] || match;
-      });
+      // Ensure sentences are capitalized after punctuation
+      msg.content = msg.content.replace(/([.!?])\s*(\w)/g, (match) =>
+        match.toUpperCase()
+      );
+
+      // Ensure the first character of the entire message is capitalized
+      if (!msg.content.startsWith("http")) {
+        msg.content =
+          msg.content.charAt(0).toUpperCase() + msg.content.slice(1);
+      }
     }
-
-    if (/[A-Z0-9]/i.test(msg.content.charAt(msg.content.length - 1))) {
-      if (!msg.content.startsWith("http", msg.content.lastIndexOf(" ") + 1))
-        msg.content += ".";
-    }
-
-    // Ensure sentences are capitalized after punctuation
-    msg.content = msg.content.replace(/([.!?])\s*(\w)/g, (match) =>
-      match.toUpperCase()
-    );
-
-    // Ensure the first character of the entire message is capitalized
-    if (!msg.content.startsWith("http")) {
-      msg.content = msg.content.charAt(0).toUpperCase() + msg.content.slice(1);
-    }
-  }
+  };
+  return presendObject;
 };
 
 export default definePlugin({
@@ -55,10 +52,15 @@ export default definePlugin({
   description: "Automatic punctuation, capitalization, and word replacement.",
   authors: [Devs.unstream],
   dependencies: ["MessageEventsAPI"],
-  start() {
-    addPreSendListener(presendObject);
+  async start() {
+    let dictionary = await fetch(
+      "https://wont-stream.github.io/dictionary/index.min.json"
+    );
+    dictionary = await dictionary.json();
+
+    addPreSendListener(getPresend(dictionary));
   },
   stop() {
-    removePreSendListener(presendObject);
+    removePreSendListener(getPresend({}));
   },
 });
